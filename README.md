f2be46a632fa66f7f1e40073c23e7b49ea0aadde
Edwing123 <egarciavalle2014@gmail.com>
2023-02-01 22:21:08 -0600

    initial commit!

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Docs - Quickstart
+
+Learning React by following the [quickstart tutorial](https://beta.reactjs.org/learn)
+from its official website.
+
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+export default function App() {
+    return (
+        <>
+            <header>
+                <a href="/">React Quickstart</a>
+            </header>
+
+            <main>
+                <h1>Learning React</h1>
+            </main>
+        </>
+    )
+}
